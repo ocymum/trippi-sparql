de7c18d18dca9e13433d21ba63766ac93b8261e3
ajs6f <ajs6f@virginia.edu>
2015-08-28 11:51:26 -0400

    Slightly stronger tests

--- src/test/java/com/asoroka/trippi/impl/sparqlupdate/integration/NormalOperationIT.java
+++ src/test/java/com/asoroka/trippi/impl/sparqlupdate/integration/NormalOperationIT.java
@@ -4,27 +4,30 @@ package com.asoroka.trippi.impl.sparqlupdate.integration;
 import static com.asoroka.trippi.impl.sparqlupdate.converters.TripleConverter.tripleConverter;
 import static java.lang.Integer.parseInt;
 import static java.lang.System.getProperty;
-import static java.net.URI.create;
-import static java.util.Arrays.asList;
 import static org.apache.jena.ext.com.google.common.collect.ImmutableMap.of;
 import static org.apache.jena.query.QueryExecutionFactory.sparqlService;
 import static org.apache.jena.rdf.model.ModelFactory.createDefaultModel;
 import static org.apache.jena.riot.web.HttpOp.execHttpPostForm;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.trippi.impl.RDFFactories.createResource;
+import static org.openrdf.rio.RDFFormat.NTRIPLES;
+import static org.openrdf.rio.Rio.createParser;
 import static org.trippi.impl.RDFFactories.createTriple;
 
-import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.sparql.engine.http.Params;
-import org.jrdf.graph.GraphElementFactoryException;
 import org.jrdf.graph.Triple;
 import org.junit.Test;
-import org.trippi.TrippiException;
+import org.openrdf.model.Statement;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.helpers.StatementCollector;
+import org.trippi.TriplestoreWriter;
 
 import com.asoroka.trippi.impl.sparqlupdate.SparqlUpdateConnector;
 
@@ -45,7 +48,7 @@ public class NormalOperationIT {
     private final int PORT = parseInt(getProperty(FUSEKI_PORT_PROPERTY, "3030"));
 
     @Test
-    public void testNormalOperation() throws TrippiException, GraphElementFactoryException, IOException {
+    public void testNormalOperation() throws Exception {
 
         final String fusekiUrl = "http://localhost:" + PORT + "/jena-fuseki-war/";
 
@@ -62,26 +65,39 @@ public class NormalOperationIT {
         final Map<String, String> config = of("sparqlUpdateEndpoint", datasetUrl + "/update");
         sparqlUpdateConnector.setConfiguration(config);
 
-        // create some simple sample triples
-        final List<Triple> triples = asList(createTriple(createResource(create("info:subject1")), createResource(create(
-                "info:predicate1")), createResource(create("info:object1"))), createTriple(createResource(create(
-                        "info:subject2")), createResource(create("info:predicate2")), createResource(create(
-                                "info:object2"))));
-        final Model statements = createDefaultModel();
-        triples.stream().map(tripleConverter::convert).map(statements::asStatement).forEach(statements::add);
+        // load some simple sample triples
+        final List<Triple> triples = new ArrayList<>();
+        final RDFParser parser = createParser(NTRIPLES);
+        final StatementCollector handler = new StatementCollector();
+        parser.setRDFHandler(handler);
+        try (InputStream rdf = getClass().getResourceAsStream("/normaltestdata.nt")) {
+            parser.parse(rdf, "");
+        }
+        for (final Statement s : handler.getStatements()) {
+            triples.add(createTriple(s.getSubject(), s.getPredicate(), s.getObject()));
+        }
+
+        final Model jenaStatements = createDefaultModel();
+        triples.stream().map(tripleConverter::convert).map(jenaStatements::asStatement).forEach(jenaStatements::add);
 
         // add them to our triplestore via our SPARQL Update connector
-        sparqlUpdateConnector.getWriter().add(triples, true);
+        final TriplestoreWriter writer = sparqlUpdateConnector.getWriter();
+        // this is a write-only connector
+        assertEquals(0, writer.listTripleLanguages().length);
+        assertEquals(0, writer.listTupleLanguages().length);
+
+        writer.add(triples, true);
 
         // check that they were all added
         Model results = sparqlService(datasetUrl + "/query", ALL_TRIPLES).execConstruct();
-        assertTrue(results.containsAll(statements));
+        assertTrue(results.containsAll(jenaStatements));
 
         // now delete them from our triplestore via our SPARQL Update connector
-        sparqlUpdateConnector.getWriter().delete(triples, true);
+        writer.delete(triples, true);
 
         // check that they were all removed
         results = sparqlService(datasetUrl + "/query", ALL_TRIPLES).execConstruct();
-        assertFalse(results.containsAny(statements));
+        assertFalse(results.containsAny(jenaStatements));
+        sparqlUpdateConnector.close();
     }
 }
