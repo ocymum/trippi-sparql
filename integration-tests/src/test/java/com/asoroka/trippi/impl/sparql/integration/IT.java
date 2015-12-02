b7ac6afd6ad926c873400839369820aef13e6c4d
ajs6f <ajs6f@virginia.edu>
2015-12-02 14:29:45 -0500

    More logging foibles

--- integration-tests/src/test/java/com/asoroka/trippi/impl/sparql/integration/IT.java
+++ integration-tests/src/test/java/com/asoroka/trippi/impl/sparql/integration/IT.java
@@ -1,16 +1,9 @@
 package com.asoroka.trippi.impl.sparql.integration;
 
-import static com.yourmediashelf.fedora.client.request.FedoraRequest.setDefaultClient;
 import static java.lang.Integer.parseInt;
 import static java.lang.System.getProperty;
 
-import java.net.MalformedURLException;
-
 import org.junit.Assert;
-import org.junit.BeforeClass;
-
-import com.yourmediashelf.fedora.client.FedoraClient;
-import com.yourmediashelf.fedora.client.FedoraCredentials;
 
 public abstract class IT extends Assert {
 
@@ -18,9 +11,4 @@ public abstract class IT extends Assert {
 
 	public final static int PORT = parseInt(getProperty(CONTAINER_PORT_PROPERTY, "8080"));
 
-	@BeforeClass
-	public static void setUp() throws MalformedURLException {
-		final String baseUrl = "http://localhost:" + PORT + "/fedora";
-		setDefaultClient(new FedoraClient(new FedoraCredentials(baseUrl, "fedoraAdmin", "fc")));
-	}
 }
