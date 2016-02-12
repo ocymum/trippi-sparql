3aa7c046669998b2421ab9379c51fbc1ed6fbcbe
ajs6f <ajs6f@virginia.edu>
2016-02-12 15:06:09 -0500

    Adding configuration and logic for using a named graph

--- integration-tests/src/test/java/edu/si/trippi/impl/sparql/integration/FedoraIT.java
+++ integration-tests/src/test/java/edu/si/trippi/impl/sparql/integration/FedoraIT.java
@@ -4,12 +4,14 @@ package edu.si.trippi.impl.sparql.integration;
 import static java.net.URLEncoder.encode;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
+import static org.apache.jena.graph.NodeFactory.createURI;
 import static org.apache.jena.rdf.model.ResourceFactory.createPlainLiteral;
 import static org.apache.jena.rdf.model.ResourceFactory.createResource;
 import static org.apache.jena.riot.Lang.NTRIPLES;
 import static org.apache.jena.riot.RDFDataMgr.loadModel;
 import static org.apache.jena.riot.web.HttpOp.execHttpDelete;
 import static org.apache.jena.riot.web.HttpOp.execHttpPost;
+import static org.apache.jena.sparql.util.FmtUtils.stringForNode;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
@@ -28,6 +30,8 @@ public class FedoraIT extends IT {
     private static final String FEDORA_URI = "http://localhost:" + PORT + "/trippi-sparql-fcrepo-webapp";
 
     private static final String RI_URI = FEDORA_URI + "/risearch";
+
+    private static final String graphName = stringForNode(createURI("#ri"));
 
     final List<Resource> systemObjectNames = asList("<info:fedora/fedora-system:ServiceDeployment-3.0>",
                     "<info:fedora/fedora-system:ServiceDefinition-3.0>", "<info:fedora/fedora-system:FedoraObject-3.0>",
@@ -65,9 +69,11 @@ public class FedoraIT extends IT {
         assertTrue(triples.isEmpty());
     }
 
-    private static Model triplesForObjectName(final Resource name) {
+    private static Model triplesForObjectName(final Resource r) {
         try {
-            final String query = encode("CONSTRUCT { " + name + " ?p ?o } WHERE { " + name + " ?p ?o }", "UTF8");
+            final String queryString = "CONSTRUCT { " + r + " ?p ?o } WHERE { GRAPH " + graphName + " { " + r +
+                            " ?p ?o } . }";
+            final String query = encode(queryString, "UTF8");
             final String queryUri = RI_URI + "?type=triples&lang=sparql&format=N-Triples&query=" + query;
             return loadModel(queryUri, NTRIPLES);
         } catch (final UnsupportedEncodingException e) {
--- trippi-sparql/src/main/java/edu/si/trippi/impl/sparql/SparqlConnector.java
+++ trippi-sparql/src/main/java/edu/si/trippi/impl/sparql/SparqlConnector.java
@@ -2,7 +2,10 @@
 package edu.si.trippi.impl.sparql;
 
 import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
+import static org.apache.jena.graph.NodeFactory.createURI;
 import static org.apache.jena.riot.web.HttpOp.setDefaultHttpClient;
+import static org.apache.jena.sparql.util.FmtUtils.stringForNode;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.IOException;
@@ -10,6 +13,7 @@ import java.util.Map;
 
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.jena.graph.Node;
 import org.jrdf.graph.GraphElementFactory;
 import org.slf4j.Logger;
 import org.trippi.RDFUtil;
@@ -31,123 +35,137 @@ import org.trippi.io.TripleIteratorFactory;
  */
 public class SparqlConnector extends TriplestoreConnector {
 
-	private static final Logger log = getLogger(SparqlConnector.class);
+    private static final Logger log = getLogger(SparqlConnector.class);
 
-	public static final String DEFAULT_BUFFER_FLUSH_BATCH_SIZE = "20000";
+    public static final String BASE_URI = "info:edu.si.fedora";
 
-	public static final String DEFAULT_BUFFER_SAFE_CAPACITY = "40000";
+    /**
+     * In order to resolve relative URIs in a repeatable way, this method should always be used to provide a BASE_URI
+     * declaration.
+     * 
+     * @param text a SPARQL Query or Update passage
+     * @return the same test with a BASE_URI declaration prefixed
+     */
+    public static String rebase(String text) {
+        return format("BASE <" + BASE_URI + ">\n%1$s", text);
+    }
 
-	public static final String DEFAULT_AUTO_FLUSH_BUFFER_SIZE = "20000";
+    public static final String DEFAULT_BUFFER_FLUSH_BATCH_SIZE = "20000";
 
-	public static final String DEFAULT_AUTO_FLUSH_DORMANT_SECONDS = "5";
+    public static final String DEFAULT_BUFFER_SAFE_CAPACITY = "40000";
 
-	public static final String DEFAULT_MAX_HTTP_CONNECTIONS = "10";
+    public static final String DEFAULT_AUTO_FLUSH_BUFFER_SIZE = "20000";
 
-	private Map<String, String> config;
+    public static final String DEFAULT_AUTO_FLUSH_DORMANT_SECONDS = "5";
 
-	private SparqlSessionFactory factory;
+    public static final String DEFAULT_MAX_HTTP_CONNECTIONS = "10";
 
-	private TripleIteratorFactory tripleIteratorFactory;
+    private Map<String, String> config;
 
-	private TriplestoreWriter writer;
+    private SparqlSessionFactory factory;
 
-	private final GraphElementFactory elementFactory = new RDFUtil();
+    private TripleIteratorFactory tripleIteratorFactory;
 
-	@Override
-	public void setConfiguration(final Map<String, String> configuration) throws TrippiException {
-		config = configuration;
-		open();
-	}
+    private TriplestoreWriter writer;
 
-	@Override
-	public Map<String, String> getConfiguration() {
-		return config;
-	}
+    private final GraphElementFactory elementFactory = new RDFUtil();
 
-	@Override
-	public void setTripleIteratorFactory(final TripleIteratorFactory factory) {
-		if (tripleIteratorFactory != null) {
-			tripleIteratorFactory.shutdown();
-		}
-		tripleIteratorFactory = factory;
-		try {
-			open();
-		} catch (final TrippiException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    @Override
+    public void setConfiguration(final Map<String, String> configuration) throws TrippiException {
+        config = configuration;
+        open();
+    }
 
-	@SuppressWarnings("deprecation")
-	@Deprecated
-	@Override
-	public void init(final Map<String, String> configuration) throws TrippiException {
-		setConfiguration(configuration);
-	}
+    @Override
+    public Map<String, String> getConfiguration() {
+        return config;
+    }
 
-	@Override
-	public TriplestoreReader getReader() {
-		return getWriter();
-	}
+    @Override
+    public void setTripleIteratorFactory(final TripleIteratorFactory factory) {
+        if (tripleIteratorFactory != null) {
+            tripleIteratorFactory.shutdown();
+        }
+        tripleIteratorFactory = factory;
+        try {
+            open();
+        } catch (final TrippiException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@Override
-	public TriplestoreWriter getWriter() {
-		return writer;
-	}
+    @Deprecated
+    @Override
+    public void init(final Map<String, String> configuration) throws TrippiException {
+        setConfiguration(configuration);
+    }
 
-	@Override
-	public GraphElementFactory getElementFactory() {
-		return elementFactory;
-	}
+    @Override
+    public TriplestoreReader getReader() {
+        return getWriter();
+    }
 
-	@Override
-	public void open() throws TrippiException {
-		if (writer != null) {
-			writer.close();
-		}
-		final int bufferFlushBatchSize = parseInt(config.getOrDefault("bufferFlushBatchSize",
-				DEFAULT_BUFFER_FLUSH_BATCH_SIZE));
-		final int bufferSafeCapacity = parseInt(config.getOrDefault("bufferSafeCapacity",
-				DEFAULT_BUFFER_SAFE_CAPACITY));
-		final int autoFlushBufferSize = parseInt(config.getOrDefault("autoFlushBufferSize",
-				DEFAULT_AUTO_FLUSH_BUFFER_SIZE));
-		final int autoFlushDormantSeconds = parseInt(config.getOrDefault("autoFlushDormantSeconds",
-				DEFAULT_AUTO_FLUSH_DORMANT_SECONDS));
-		final String updateEndpoint = config.get("updateEndpoint");
-		log.info("Using update endpoint {}", updateEndpoint);
-		final String queryEndpoint = config.getOrDefault("queryEndpoint", updateEndpoint);
-		log.info("Using query endpoint {}", queryEndpoint);
-		final String constructEndpoint = config.getOrDefault("constructEndpoint", queryEndpoint);
-		log.info("Using construct endpoint {}", constructEndpoint);
+    @Override
+    public TriplestoreWriter getWriter() {
+        return writer;
+    }
 
-		if (factory != null) {
-			factory.close();
-		}
-		factory = new SparqlSessionFactory(updateEndpoint, queryEndpoint, constructEndpoint);
+    @Override
+    public GraphElementFactory getElementFactory() {
+        return elementFactory;
+    }
 
-		final PoolingClientConnectionManager connectionManager = new PoolingClientConnectionManager();
-		final int maxConnections = parseInt(config.getOrDefault("maxHttpConnections", DEFAULT_MAX_HTTP_CONNECTIONS));
-		connectionManager.setMaxTotal(maxConnections);
-		connectionManager.setDefaultMaxPerRoute(maxConnections);
-		setDefaultHttpClient(new DefaultHttpClient(connectionManager));
+    @Override
+    public void open() throws TrippiException {
+        if (writer != null) {
+            writer.close();
+        }
+        final int bufferFlushBatchSize = parseInt(config.getOrDefault("bufferFlushBatchSize",
+                        DEFAULT_BUFFER_FLUSH_BATCH_SIZE));
+        final int bufferSafeCapacity = parseInt(config.getOrDefault("bufferSafeCapacity",
+                        DEFAULT_BUFFER_SAFE_CAPACITY));
+        final int autoFlushBufferSize = parseInt(config.getOrDefault("autoFlushBufferSize",
+                        DEFAULT_AUTO_FLUSH_BUFFER_SIZE));
+        final int autoFlushDormantSeconds = parseInt(config.getOrDefault("autoFlushDormantSeconds",
+                        DEFAULT_AUTO_FLUSH_DORMANT_SECONDS));
+        final String updateEndpoint = config.get("updateEndpoint");
+        log.info("Using update endpoint {}", updateEndpoint);
+        final String queryEndpoint = config.getOrDefault("queryEndpoint", updateEndpoint);
+        log.info("Using query endpoint {}", queryEndpoint);
+        final String constructEndpoint = config.getOrDefault("constructEndpoint", queryEndpoint);
+        log.info("Using construct endpoint {}", constructEndpoint);
+        final Node graphName = createURI(config.getOrDefault("graphName", "#ri"));
+        log.info("Using graph name {}", stringForNode(graphName));
 
-		if (tripleIteratorFactory == null) {
-			tripleIteratorFactory = new TripleIteratorFactory();
-		}
+        if (factory != null) {
+            factory.close();
+        }
+        factory = new SparqlSessionFactory(updateEndpoint, queryEndpoint, constructEndpoint, graphName);
 
-		final ConfigurableSessionPool pool = new ConfigurableSessionPool(factory, 1, 5, 1);
-		final UpdateBuffer buffer = new MemUpdateBuffer(bufferFlushBatchSize, bufferSafeCapacity);
-		try {
-			writer = new ConcurrentTriplestoreWriter(pool, new DefaultAliasManager(), factory.newSession(), buffer,
-					tripleIteratorFactory, autoFlushBufferSize, autoFlushDormantSeconds);
-		} catch (final IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        final PoolingClientConnectionManager connectionManager = new PoolingClientConnectionManager();
+        final int maxConnections = parseInt(config.getOrDefault("maxHttpConnections", DEFAULT_MAX_HTTP_CONNECTIONS));
+        connectionManager.setMaxTotal(maxConnections);
+        connectionManager.setDefaultMaxPerRoute(maxConnections);
+        setDefaultHttpClient(new DefaultHttpClient(connectionManager));
 
-	@Override
-	public void close() throws TrippiException {
-		writer.close();
-		factory.close();
-		tripleIteratorFactory.shutdown();
-	}
+        if (tripleIteratorFactory == null) {
+            tripleIteratorFactory = new TripleIteratorFactory();
+        }
+
+        final ConfigurableSessionPool pool = new ConfigurableSessionPool(factory, 1, 5, 1);
+        final UpdateBuffer buffer = new MemUpdateBuffer(bufferFlushBatchSize, bufferSafeCapacity);
+        try {
+            writer = new ConcurrentTriplestoreWriter(pool, new DefaultAliasManager(), factory.newSession(), buffer,
+                            tripleIteratorFactory, autoFlushBufferSize, autoFlushDormantSeconds);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() throws TrippiException {
+        writer.close();
+        factory.close();
+        tripleIteratorFactory.shutdown();
+    }
 }
